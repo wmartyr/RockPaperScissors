5b181e50f58cc582c3424742fdfa7dff528c3e87
Woodrow Martyr <woodrow.martyr@gmail.com>
2023-12-28 12:28:27 +1030

    Implement basic layout

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["✊", "✋", "✌️"]
+
+    @State private var appMove = Int.random(in: 0..<3)
+    @State private var shouldWin = Bool.random()
+    @State private var playerMove = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text("App chooses \(moves[appMove])")
+            Text("You should \(shouldWin ? "win" : "lose")")
+            Text("Your choice")
+            HStack {
+                ForEach(0..<moves.count, id: \.self) {
+                    Button(moves[$0]) {
+                        print("Button pressed")
+                    }
+                }
+            }
         }
         .padding()
     }
